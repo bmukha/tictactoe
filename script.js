40bf502ece70b564d813973be4520e1d4c5def7b
bmukha <47007157+bmukha@users.noreply.github.com>
2020-11-04 18:09:20 +0200

    terribly done

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const gameBoard = (() => {
+const PlayerFactory = (name, sign) => {
+  return {
+    name,
+    sign,
+  };
+};
+
+const gameboard = (() => {
   const startBtn = document.querySelector("#start");
-  startBtn.addEventListener("click", () => gameFlow.start());
+  startBtn.addEventListener("click", () => game.start());
   const resetBtn = document.querySelector("#reset");
-  resetBtn.addEventListener("click", () => gameFlow.reset());
+  resetBtn.addEventListener("click", () => game.reset());
+
+  const playersSigns = () => {
+    const selection = document.getElementsByName("sign");
+    if (selection[0].checked) {
+      return {
+        p1: "X",
+        p2: "O",
+      };
+    }
+    return {
+      p1: "O",
+      p2: "X",
+    };
+  };
+
+  const player1Name = () => document.querySelector("#p1name").value;
+  const player2Name = () => document.querySelector("#p2name").value;
 
   const gameState = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN];
 
@@ -19,64 +43,96 @@ const gameBoard = (() => {
   return {
     gameState,
     render,
+    playersSigns,
+    player1Name,
+    player2Name,
   };
 })();
 
-const PlayerFactory = () => {
-  let name = prompt("Enter your name");
-  return {
-    name,
-  };
-};
+const game = (() => {
+  const board = document.querySelector("#board");
 
-const gameFlow = (() => {
-  let gameState = "start";
-  let playerSign = "X";
+  const initPlayers = () => {
+    const player1 = PlayerFactory(
+      gameboard.player1Name(),
+      gameboard.playersSigns().p1
+    );
+    const player2 = PlayerFactory(
+      gameboard.player2Name(),
+      gameboard.playersSigns().p2
+    );
+    return {
+      player1,
+      player2,
+    };
+  };
+
   const start = () => {
-    let board = document.querySelector("#board");
-    board.addEventListener("click", (e) => {
-      if (e.target.id[0] === "f" && !e.target.innerHTML) {
-        gameBoard.gameState[e.target.id.slice(1) - 1] = gameFlow.changeSign();
-        console.log(e.target.id.slice(1));
-        console.log(gameBoard.gameState);
-        console.log(checkWinCondition());
-        gameBoard.render();
-        setTimeout(() => {
-          if (checkWinCondition()) {
-            alert("Game Over!");
-            gameFlow.reset();
-          }
-        }, 10);
+    if (gameboard.player1Name() === "" || gameboard.player2Name() === "") {
+      alert("Fill Player1 and Player2 names first!");
+      return;
+    }
+    board.addEventListener("click", play);
+    // gameboard.render();
+  };
+
+  let activePlayer;
+
+  const play = (e) => {
+    const players = initPlayers();
+
+    if (e.target.id[0] === "f" && !e.target.innerHTML) {
+      if (activePlayer === undefined) {
+        activePlayer = players.player1;
       }
-    });
-    gameBoard.render();
+      gameboard.gameState[e.target.id.slice(1) - 1] = activePlayer.sign;
+      gameboard.render();
+
+      setTimeout(() => {
+        if (checkWinCondition()) {
+          alert(`Game Over! ${activePlayer.name} won!`);
+          game.reset();
+          return;
+        } else if (!gameboard.gameState.includes(NaN)) {
+          alert(`It's a draw!`);
+          game.reset();
+          return;
+        }
+      }, 10);
+      if (activePlayer.name === players.player1.name) {
+        activePlayer = players.player2;
+      } else {
+        activePlayer = players.player1;
+      }
+    }
   };
+
   const reset = () => {
-    gameBoard.gameState.fill(NaN);
-    console.log(gameBoard.gameState);
-    gameBoard.render();
+    game.playerCount = 0;
+    gameboard.gameState.fill(NaN);
+    log.innerHTML = "";
+    gameboard.render();
+    activePlayer = undefined;
   };
-  const changeSign = () =>
-    playerSign === "X" ? (playerSign = "O") : (playerSign = "X");
 
   const checkWinCondition = () => {
     if (
-      (gameBoard.gameState[0] === gameBoard.gameState[1] &&
-        gameBoard.gameState[0] === gameBoard.gameState[2]) ||
-      (gameBoard.gameState[3] === gameBoard.gameState[4] &&
-        gameBoard.gameState[3] === gameBoard.gameState[5]) ||
-      (gameBoard.gameState[6] === gameBoard.gameState[7] &&
-        gameBoard.gameState[6] === gameBoard.gameState[8]) ||
-      (gameBoard.gameState[0] === gameBoard.gameState[3] &&
-        gameBoard.gameState[0] === gameBoard.gameState[6]) ||
-      (gameBoard.gameState[1] === gameBoard.gameState[4] &&
-        gameBoard.gameState[1] === gameBoard.gameState[7]) ||
-      (gameBoard.gameState[2] === gameBoard.gameState[5] &&
-        gameBoard.gameState[2] === gameBoard.gameState[8]) ||
-      (gameBoard.gameState[0] === gameBoard.gameState[4] &&
-        gameBoard.gameState[0] === gameBoard.gameState[8]) ||
-      (gameBoard.gameState[2] === gameBoard.gameState[4] &&
-        gameBoard.gameState[2] === gameBoard.gameState[6])
+      (gameboard.gameState[0] === gameboard.gameState[1] &&
+        gameboard.gameState[0] === gameboard.gameState[2]) ||
+      (gameboard.gameState[3] === gameboard.gameState[4] &&
+        gameboard.gameState[3] === gameboard.gameState[5]) ||
+      (gameboard.gameState[6] === gameboard.gameState[7] &&
+        gameboard.gameState[6] === gameboard.gameState[8]) ||
+      (gameboard.gameState[0] === gameboard.gameState[3] &&
+        gameboard.gameState[0] === gameboard.gameState[6]) ||
+      (gameboard.gameState[1] === gameboard.gameState[4] &&
+        gameboard.gameState[1] === gameboard.gameState[7]) ||
+      (gameboard.gameState[2] === gameboard.gameState[5] &&
+        gameboard.gameState[2] === gameboard.gameState[8]) ||
+      (gameboard.gameState[0] === gameboard.gameState[4] &&
+        gameboard.gameState[0] === gameboard.gameState[8]) ||
+      (gameboard.gameState[2] === gameboard.gameState[4] &&
+        gameboard.gameState[2] === gameboard.gameState[6])
     ) {
       return true;
     }
@@ -84,10 +140,7 @@ const gameFlow = (() => {
   };
 
   return {
-    gameState,
     start,
     reset,
-    changeSign,
-    checkWinCondition,
   };
 })();
